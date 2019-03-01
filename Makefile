0042104351f4181e50f616ba0ce082c86ce18bdd
Mario <mario.li@foxmail.com>
2019-03-01 09:49:10 +0800

    New volumn structure

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:master.tex *.tex
 	mkdir -p aux
-	mkdir -p output/src
+	mkdir -p output/src/vol1 output/src/vol2
 	xelatex -aux_directory=aux -output-directory=output master
 	xelatex -aux_directory=aux -output-directory=output master
 	xelatex -aux_directory=aux -output-directory=output master
--- genmaster.pl
+++ genmaster.pl
@@ -8,8 +8,17 @@ open my $fh, "<$file" or die $!;
 my $contents=<$fh>;
 close $fh;
 
-my @files=map{s/.tex$//;$_} sort glob("src/ch*.tex");
-my $files=join("\n", map{"\\include{$_}"} @files);
-$contents=~s/%<<T_FILES>>\\include\{T_FILE\}<<\/T_FILES>>\K/\n$files/x;
+# my @files=map{s/.tex$//;$_} sort glob("src/ch*.tex");
+# my $files=join("\n", map{"\\include{$_}"} @files);
+
+# my @match = ($contents=~/\\include{(.*?)}/);
+
+$contents=~s/%<<T_FILES>>\\include\{(.*?)\}<<\/T_FILES>>\K/list_files($1)/exg;
 print $contents;
 
+
+sub list_files{
+  my ($pattern)=@_;
+  my @files = glob($pattern);
+  return "\n" . join("\n", map{s/.tex$//;"\\include{$_}"} @files);
+}
